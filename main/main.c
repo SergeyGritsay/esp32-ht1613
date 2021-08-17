c19085070f04f8ce7a4d21bc00af5ea77329b53b
SergeyGritsay <Sergey-Gritsay2018@yandex.ru>
2021-08-17 16:15:22 +0300

    TEST LED CONTROL

--- main/main.c
+++ main/main.c
@@ -4,32 +4,16 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "driver/periph_ctrl.h"
+#include "esp_log.h"
+#include "time.h"
 
 #define data_LED 17
 
 
 void set_1MHz_clock_on_GPIO18(void)
 {
-	periph_module_enable(PERIPH_LEDC_MODULE);
-
-   // Set up timer
-	ledc_timer_config_t ledc_timer = {
-     // We need clock, not PWM so 1 bit is enough.
-		.duty_resolution = LEDC_TIMER_1_BIT,
-
-     // Clock frequency, 1 MHz, high speed
-		.freq_hz = 1000000,
-		.speed_mode = LEDC_HIGH_SPEED_MODE,
-		.timer_num = LEDC_TIMER_0,
-
-     // I think not needed for new esp-idf software, try uncommenting
-		.clk_cfg = LEDC_USE_APB_CLK
-	};
-
-	ledc_timer_config(&ledc_timer); // Set up GPIO PIN 
-
 	gpio_config_t io_conf = {
-		.intr_type       = GPIO_PIN_INTR_DISABLE,
+		.intr_type    = GPIO_PIN_INTR_DISABLE,
 		.mode         = GPIO_MODE_INPUT_OUTPUT_OD,
 		.pin_bit_mask = ( 1ULL << GPIO_NUM_18),
 		.pull_down_en = 0,
@@ -37,7 +21,21 @@ void set_1MHz_clock_on_GPIO18(void)
 	};
    
 	gpio_config(&io_conf);
-   
+	
+	periph_module_enable(PERIPH_LEDC_MODULE);
+
+   // Set up timer
+	ledc_timer_config_t ledc_timer = {
+    
+		.duty_resolution = LEDC_TIMER_1_BIT,
+
+    	.freq_hz = 1000000,
+		.speed_mode = LEDC_HIGH_SPEED_MODE,
+		.timer_num = LEDC_TIMER_0
+    };
+
+	ledc_timer_config(&ledc_timer); // Set up GPIO PIN 
+	   
 	ledc_channel_config_t channel_config = {
 		.channel    = LEDC_CHANNEL_0,
 		.duty       = 1,
@@ -48,51 +46,64 @@ void set_1MHz_clock_on_GPIO18(void)
 	
 	ledc_channel_config(&channel_config);
 };
-/* const uint8_t data_set [10]={
-	0b0000,
-	0b0001,
-	0b0010,
-	0b0011,
-	0b0100,
-	0b0101,
-	0b0110,
-	0b0111,
-	0b1000,
-	0b1001
-}; */
+
+/* data example 
+	0b0000, 0
+	0b0001, 1
+	0b0010, 2
+	0b0011, 3
+	0b0100, 4
+	0b0101, 5
+	0b0110, 6
+	0b0111, 7
+	0b1000, 8
+	0b1001  9
+*/
+
+void ht1613_task(void * arg)
+{
+	
+}
+void set_level(int gpio_output){
+	gpio_set_direction(gpio_output, GPIO_MODE_INPUT_OUTPUT);
+	int clock_level = gpio_get_level(gpio_output);
+	gpio_set_level(gpio_output, !clock_level);
+	clock_level = gpio_get_level(gpio_output);
+	//printf("%d\n",clock_level);
+	//vTaskDelay(1000/portTICK_PERIOD_MS);
+}
 
 void app_main(void){
+	//int time = 0;
+	//int time_1 = 0;
 	uint8_t data = 0b0000;
-	//_74hc595_test();
-	//int data = 0x0000;
-	gpio_config_t io_conf;
-	io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-	io_conf.mode = GPIO_MODE_INPUT_OUTPUT_OD;
-	io_conf.pin_bit_mask = (1ULL << data_LED);
-	io_conf.pull_down_en = 0;
-	io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-	//set_1MHz_clock_on_GPIO2();
-	set_1MHz_clock_on_GPIO18();
-	int clock_level = gpio_get_level(GPIO_NUM_18);
-	//printf("%d\n",clock_level);
-    // Main loop
-	int i=0;
-    while(1) {
-		gpio_set_level(GPIO_NUM_18, 1);
-		clock_level = gpio_get_level(GPIO_NUM_18);
-		if ((clock_level == 0) && (i != 10)){
-			printf("%d",data);
-			data += 1;
-			i += 1;
-			vTaskDelay(100);
-		} else{
-			printf("\n");
-			printf("thats all\n");
-			break;
-		}
-		//gpio_set_level(GPIO_NUM_18, 1);
-		//printf("%d\n", data);
+	gpio_config_t io_conf = {
+		.intr_type    = GPIO_PIN_INTR_DISABLE,
+		.mode         = GPIO_MODE_INPUT_OUTPUT_OD,
+		.pin_bit_mask = (1ULL << data_LED),
+		.pull_down_en = 0,
+		.pull_up_en   = 0,
 	};
-	//printf("\n");
+    gpio_config(&io_conf);
+	set_1MHz_clock_on_GPIO18();
+	//set_level(GPIO_NUM_18);
+	//set_level(GPIO_NUM_18);
+	// Main loop
+    while(1) {
+		//printf("%d\n", clock_level);
+		if ((gpio_get_level(GPIO_NUM_18) == 0) && (data <= 0b1001)){
+			//set_level(GPIO_NUM_18);
+			printf("%d\n",data);
+			set_level(GPIO_NUM_18);
+			data += 1;
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		} else {if (data<= 0b1001){
+			//printf("thats all\n");//?????????????????????
+			set_level(GPIO_NUM_18);
+			vTaskDelay(1000/ portTICK_PERIOD_MS);
+			//break;
+		} else {
+		break;
+		};};
+		};	
 } 
