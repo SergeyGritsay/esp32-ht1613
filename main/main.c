431df18dc78d13f9883a11b907fb14acc5cf548b
SergeyGritsay <Sergey-Gritsay2018@yandex.ru>
2021-08-18 15:31:56 +0300

    TEST LED CONTROL #2

--- main/main.c
+++ main/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <math.h>
+#include <esp32/rom/ets_sys.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "driver/periph_ctrl.h"
 #include "esp_log.h"
-#include "time.h"
 
-#define data_LED 17
-
+#define data_LED GPIO_NUM_17
+uint8_t data = 0b0000;
 
 void set_1MHz_clock_on_GPIO18(void)
 {
@@ -29,7 +30,7 @@ void set_1MHz_clock_on_GPIO18(void)
     
 		.duty_resolution = LEDC_TIMER_1_BIT,
 
-    	.freq_hz = 1000000,
+    	.freq_hz = 10000000,
 		.speed_mode = LEDC_HIGH_SPEED_MODE,
 		.timer_num = LEDC_TIMER_0
     };
@@ -48,35 +49,51 @@ void set_1MHz_clock_on_GPIO18(void)
 };
 
 /* data example 
-	0b0000, 0
-	0b0001, 1
-	0b0010, 2
-	0b0011, 3
-	0b0100, 4
-	0b0101, 5
-	0b0110, 6
-	0b0111, 7
-	0b1000, 8
-	0b1001  9
+	0x0, 0b0000, 0
+	0x1, 0b0001, 1
+	0x2, 0b0010, 2
+	0x3, 0b0011, 3
+	0x4, 0b0100, 4
+	0x5, 0b0101, 5
+	0x6, 0b0110, 6
+	0x7, 0b0111, 7
+	0x8, 0b1000, 8
+	0x9, 0b1001  9
 */
 
-void ht1613_task(void * arg)
-{
-	
-}
-void set_level(int gpio_output){
+void set_low_level(int gpio_output){
 	gpio_set_direction(gpio_output, GPIO_MODE_INPUT_OUTPUT);
-	int clock_level = gpio_get_level(gpio_output);
-	gpio_set_level(gpio_output, !clock_level);
+	int clock_level = 0;
+	gpio_set_level(gpio_output, clock_level);
 	clock_level = gpio_get_level(gpio_output);
-	//printf("%d\n",clock_level);
-	//vTaskDelay(1000/portTICK_PERIOD_MS);
+	//printf("level: %d\n",clock_level);
+}
+
+void set_high_level(int gpio_output){
+	gpio_set_direction(gpio_output, GPIO_MODE_INPUT_OUTPUT);
+	int clock_level = 1;
+	gpio_set_level(gpio_output, clock_level);
+	clock_level = gpio_get_level(gpio_output);
+	//printf("level: %d\n", clock_level);
+}
+
+void ht1613_send_byte(uint8_t byte)
+{
+	for (uint8_t i = 0; i < 4; i++ )
+	{	
+		if (byte == 1) {
+			set_high_level(GPIO_NUM_17);
+		} else {
+			set_low_level(GPIO_NUM_17);
+		}
+		//printf("level of data: %d\n", gpio_get_level(GPIO_NUM_17));
+		//gpio_set_level(data_LED,byte & 0b0001);
+		byte >>= 1;
+	};
+	data += 0b0001;
 }
 
 void app_main(void){
-	//int time = 0;
-	//int time_1 = 0;
-	uint8_t data = 0b0000;
 	gpio_config_t io_conf = {
 		.intr_type    = GPIO_PIN_INTR_DISABLE,
 		.mode         = GPIO_MODE_INPUT_OUTPUT_OD,
@@ -86,24 +103,25 @@ void app_main(void){
 	};
     gpio_config(&io_conf);
 	set_1MHz_clock_on_GPIO18();
-	//set_level(GPIO_NUM_18);
-	//set_level(GPIO_NUM_18);
 	// Main loop
     while(1) {
-		//printf("%d\n", clock_level);
 		if ((gpio_get_level(GPIO_NUM_18) == 0) && (data <= 0b1001)){
-			//set_level(GPIO_NUM_18);
-			printf("%d\n",data);
-			set_level(GPIO_NUM_18);
-			data += 1;
+			printf("data: %d\n", data);
+			ht1613_send_byte(data);
+			set_high_level(GPIO_NUM_18);
+			//data += 0b0001;
 			vTaskDelay(1000 / portTICK_PERIOD_MS);
-		} else {if (data<= 0b1001){
-			//printf("thats all\n");//?????????????????????
-			set_level(GPIO_NUM_18);
-			vTaskDelay(1000/ portTICK_PERIOD_MS);
-			//break;
+			//printf("%d\n", data);
 		} else {
-		break;
-		};};
+			if ((data <= 0b1001) /*|| (gpio_get_level(GPIO_NUM_18 == 1))*/){
+				//printf("thats all\n");
+				set_low_level(GPIO_NUM_18);
+				vTaskDelay(1000 / portTICK_PERIOD_MS);
+				//break;
+			} 
+		else {
+				break;
+			};
+		};
 		};	
 } 
